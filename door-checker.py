8200be15d9bb4359907654cf7326cf016366ad8f
VitorCarvalho67 <vitor.carvalho67@etec.sp.gov.br>
2023-02-04 12:46:42 -0300

    first commit

---
+++ door-checker.py
@@ -0,0 +1,16 @@
+  GNU nano 7.1                          oi.py                                    
+import socket
+
+ports = [21,22,80,443,3306,25]
+
+for port in ports:
+
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(0.3)
+        code = client.connect_ex(("etecrgs.com.br", port))
+
+        if code== 0:
+                print("porta aberta", port)
+        else:
+                print("porta fechada", port)
+
